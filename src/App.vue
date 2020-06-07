b845e6bb783c95d20246745fe668e082356dbda4
Dominique Lahl <domee.lahl@gmail.com>
2020-06-07 23:25:20 +0200

    Implement frontend task menu

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div id="nav">
+      <router-link to="/tasks">Go to Tasks</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,16 +1,38 @@
 <template>
     <div id="TaskView">
-
-        
+        <div class="container container__taskView">
+            <div class="container__element container__element--task">
+                    <input type="text" class="input input__text" form="task-form">
+                    <input type="date" class="input input__date" form="task-form">
+                    <input type="submit" class="input input__submit" form="task-form" value="+">
+            </div>
+            <div class="container__element container__element--task">
+                <input type="submit" class="input input__submit" value="✓">
+                <span class="container__text input__text text__task">{{ body }}</span>
+                <div class="divider divider__vertical"></div>
+                <label for="due-date" class="container__text text__task">Due:</label>
+                <input type="date" id="due-date" class="input input__date" disabled>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data: function()
+    {
+        return {
+            body: "This is a test taskkkkkkkkk kkkkkkkkkkkkkkkkkkkk kkkkkkkkkk kkkkkkkkkk kkkkkkkkkkkkk",
+            created: "", 
+            finished: "",
+            done: false
+        }
+    }
     
 }
 </script>
 
 <style lang="sass">
+@import "@/stylesheets/main.scss"
 
 </style>
