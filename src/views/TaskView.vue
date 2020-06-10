a113a6c316689725ad025fcb2ee23c3bc1d12097
Dominique Lahl <domee.lahl@gmail.com>
2020-06-11 01:22:49 +0200

    Restructure and rename css styling

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,19 +1,24 @@
 <template>
-    <div id="TaskView">
-        <div class="container container__taskView">
-            <div class="container__element container__element--task">
-                    <input type="text" class="input input__text" form="task-form" v-model="body">
-                    <input type="date" class="input input__date" form="task-form" v-model="finishBy">
-                    <input type="submit" class="input input__submit" form="task-form" value="+" v-on:click="createTask">
-            </div>
-            <div class="container__element container__element--task" v-for="item in filteredTasks" :key="item">
-                <button class="input input__submit input__submit--finish" v-on:click="item.done = true; editTask(item._id, item)"></button>
-                <span class="container__text text__task text__task--body">{{ item.body }}</span>
-                <div class="divider divider__vertical"></div>
-                <label for="due-date" class="container__text text__task">Due:</label>
-                <input type="date" id="due-date" class="input input__date" v-model="item.finishBy" disabled>
-                <button class="input input__submit input__submit--delete" v-on:click="deleteTask(item._id)">☓</button>
-            </div>
+    <div id="TaskView" class="taskview">
+        <div class="taskview__element taskview__input">
+            <input type="text" class="ipt ipt__text txt__ipt txt--default grow" v-model="body" v-on:keyup.enter="createTask">
+            <input type="date" class="ipt ipt__date txt__ipt txt--default" v-model="finishBy" v-on:keyup.enter="createTask">
+            <button class="btn btn--blue" v-on:click="createTask">
+                <span class="txt__btn txt--large">+</span>
+            </button>
+        </div>
+        <div class="taskview__element taskview__task" v-for="item in filteredTasks" :key="item">
+            <button class="btn btn--white" v-on:click="item.done = true; editTask(item._id, item)">
+            </button>
+            <span class="txt__task txt--default txt--large grow">{{ item.body }}</span>
+            <div class="divider__vertical"></div>
+            
+            <label class="txt__label txt--default">Due:</label>
+            <input type="date" class="ipt ipt__date txt__ipt txt--default" v-model="item.finishBy" disabled>
+            
+            <button class="btn btn--red" v-on:click="deleteTask(item._id)">
+                <span class="txt__btn txt--large">x</span>
+            </button>
         </div>
     </div>
 </template>
@@ -36,7 +41,7 @@ export default {
   computed: {
     filteredTasks: function() {
       return this.tasks.filter(i => i.done === false);
-    }
+    },
   },
   created: async function() {
     await this.fetchTasks();
