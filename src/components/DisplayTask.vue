62d5edb109b174a7c1c26f53b346222225c4431e
Dominique Lahl <domee.lahl@gmail.com>
2020-06-12 00:38:17 +0200

    Replace date on task display with "days left"

--- src/components/DisplayTask.vue
+++ src/components/DisplayTask.vue
@@ -4,10 +4,7 @@
 		</button>
 		<span class="txt__task txt--default txt--large grow">{{ item.body }}</span>
 		<div class="divider__vertical"></div>
-		
-		<label class="txt__label txt--default">Due:</label>
-		<input type="date" class="ipt ipt__date txt__ipt txt--default" v-model="item.finishBy" disabled>
-		
+		<span class="txt__task txt--default" v-if="daysLeft >= 0 && item.finishBy != ''">{{ daysLeft }} Days left</span>
 		<button class="btn btn--red" v-on:click="deleteTask()">
 			<span class="txt__btn txt--large">x</span>
 		</button>
@@ -19,6 +16,31 @@ export default {
 	name: "DisplayTask",
 	props: {
 		item: Object
+	},
+	created: function() {
+		console.log(this.item.finishBy);
+		console.log(this.item.created);
+		let finishBySeparated = this.item.finishBy.split('-');
+		let createdSeparated = this.item.created.split('-');
+
+		let diffYear = finishBySeparated[0] - createdSeparated[0];
+		let diffMonth = finishBySeparated[1] - createdSeparated[1];
+		let diffDay = finishBySeparated[2] - createdSeparated[2];
+
+		if (diffYear > 0) {
+			this.daysLeft += diffYear * 365;
+		}
+		if (diffMonth > 0) {
+			this.daysLeft += diffMonth * 30.42;
+		}
+		if (diffDay > 0) {
+			this.daysLeft += diffDay;
+		}
+	},
+	data() {
+		return {
+			daysLeft: 0,
+		}
 	},
 	methods: {
 		finishTask() {
